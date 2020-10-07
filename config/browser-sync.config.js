2b2a7e55543f0176f1c1fdcb2265640bdecfed50
Bruce Taylor <brucetaylor@Bruces-MacBook-Pro.local>
2020-10-07 10:08:49 +0100

    Browsersync config updated

--- config/browser-sync.config.js
+++ config/browser-sync.config.js
@@ -1,20 +1,16 @@
 const fs = require('fs');
 
-// A useful way to reference the website we are wanting eleventy to build / output
-let website = 'workspace-a';
-
 module.exports = {
   notify: true,
   watch: true,
   browser: ['google chrome'],
   open: 'local',
-  server: `/${website}/dist/`,
+  server: `dist/`,
   startPath: `/`,
-  logPrefix: `${website} website`,
   logLevel: 'debug',
   callbacks: {
     ready(err, browserSync) {
-      const content404 = fs.readFileSync(`/${website}/dist/404/`);
+      const content404 = fs.readFileSync(`dist/404/index.html`);
 
       browserSync.addMiddleware('*', (req, res) => {
         // Provides the 404 content without redirect.
